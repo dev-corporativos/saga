3487fac38d6b73e92ea1d5184bdf2a4c9d9958fe
Barr0ca <iandearaujogalvao@gmail.com>
2025-12-16 18:17:33 -0300

    feat: Implementa a inicialização do banco de dados e criação da tabela de pagamentos

---
+++ pagamento-service/src/database/index.ts
@@ -0,0 +1,33 @@
+import { Pool } from 'pg'
+import { config } from '../config'
+import { Pedido } from '../services/pedido.service'
+
+export const pool = new Pool({
+  host: config.database.host,
+  port: config.database.port,
+  user: config.database.user,
+  password: config.database.password,
+  database: config.database.name,
+})
+
+export const initializeDatabase = async (): Promise<void> => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS pagamentos (
+        id VARCHAR(36) PRIMARY KEY,
+        pedido_id VARCHAR(36) NOT NULL UNIQUE,
+        valor INT NOT NULL,
+        status VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+
+      CREATE INDEX IF NOT EXISTS idx_pagamentos_status ON pagamentos(status);
+      CREATE INDEX IF NOT EXISTS idx_pagamentos_pedidos ON pagamentos(pedido_id);
+    `)
+    console.log('Banco de dados de Pagamento Service inicializado.')
+  } catch (error) {
+    console.error('Erro ao inicializar o banco de dados:', error)
+    throw error
+  }
+}
