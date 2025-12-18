627312ad44132bf67a44c3c615863794f8f5d4c1
Barr0ca <iandearaujogalvao@gmail.com>
2025-12-18 16:04:39 -0300

    fix: Alterando o tipo do campo valor, valor padrão de status e adicionando o campo metodo

--- pagamento-service/src/database/index.ts
+++ pagamento-service/src/database/index.ts
@@ -1,6 +1,5 @@
-import { Pool } from 'pg'
-import { config } from '../config'
-import { Pagamento } from '../services/pagamento.service'
+import { Pool } from "pg";
+import { config } from "../config";
 
 export const pool = new Pool({
   host: config.database.host,
@@ -8,26 +7,27 @@ export const pool = new Pool({
   user: config.database.user,
   password: config.database.password,
   database: config.database.name,
-})
+});
 
 export const initializeDatabase = async (): Promise<void> => {
   try {
     await pool.query(`
-      CREATE TABLE IF NOT EXISTS pagamentos (
+      CREATE TABLE IF NOT EXISTS pagamento (
         id VARCHAR(36) PRIMARY KEY,
         pedido_id VARCHAR(36) NOT NULL UNIQUE,
-        valor INT NOT NULL,
-        status VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+        valor DECIMAL(10,2) NOT NULL,
+        metodo VARCHAR(50) NOT NULL,
+        status VARCHAR(20) NOT NULL DEFAULT 'PENDENTE',
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );
 
       CREATE INDEX IF NOT EXISTS idx_pagamentos_status ON pagamentos(status);
       CREATE INDEX IF NOT EXISTS idx_pagamentos_pedidos ON pagamentos(pedido_id);
-    `)
-    console.log('Banco de dados de Pagamento Service inicializado.')
+    `);
+    console.log("Banco de dados de Pagamento Service inicializado.");
   } catch (error) {
-    console.error('Erro ao inicializar o banco de dados:', error)
-    throw error
+    console.error("Erro ao inicializar o banco de dados:", error);
+    throw error;
   }
-}
+};
