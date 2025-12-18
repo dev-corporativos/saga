b4e9ab4eb83e1d6e585343d3f511e4fb6dac5892
Barr0ca <iandearaujogalvao@gmail.com>
2025-12-18 16:00:57 -0300

    chore: Padronizando arquivo de serviços

--- pagamento-service/src/index.ts
+++ pagamento-service/src/index.ts
@@ -1,23 +1,28 @@
-import express from 'express'
-import { config } from './config'
-import { initializeDatabase } from './database'
-import { connectRabbitMQ } from './messaging/rabbitmq'
-import { createPagamentoHandler } from './controllers/pagamento.controller'
-import { listenPagamentoEvents } from './events/pagamento.consumer'
+import { initializeDatabase } from "./database"
+import { initializeRabbitMQ } from "./messaging/rabbitmq"
+import { config } from "./config"
+import { getChannel, getConnection } from "./messaging/rabbitmq"
+import { app } from "./controllers/pagamento.controller"
 
-const app = express()
-app.use(express.json())
+const startServer = async () => {
+  try {
+    await initializeDatabase()
+    await initializeRabbitMQ()
 
-app.post('/pagamentos', createPagamentoHandler)
-
-async function start() {
-  await initializeDatabase()
-  await connectRabbitMQ()
-  await listenPagamentoEvents()
-
-  app.listen(config.service.port, () => {
-    console.log(`${config.service.name} rodando na porta ${config.service.port}`)
-  })
+    app.listen(config.service.port, () => {
+      console.log(`[Pagamento Service] Servidor rodando na porta ${config.service.port}`)
+    })
+  } catch (error) {
+    console.error('Erro ao iniciar o serviço:', error)
+    process.exit(1)
+  }
 }
 
-start()
+startServer()
+
+process.on('SIGINT', async () => {
+  console.log('[Pagamento Service] Encerrando o serviço...')
+  if (getChannel()) await getChannel().close()
+  if (getConnection()) await getConnection().close()
+  process.exit(0)
+})
