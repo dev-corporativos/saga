95e7b2ae6ab63c58438cf6e92e4b17b77e74ce21
Jardson Alan <jardsonalan132@gmail.com>
2025-12-19 10:07:21 -0300

    refactor: Remove ouvinte de eventos não utilizado e simplifica inicialização do RabbitMQ

--- entrega-service/src/messaging/rabbitmq.ts
+++ entrega-service/src/messaging/rabbitmq.ts
@@ -1,6 +1,6 @@
 import * as amqp from 'amqplib'
 import { config } from '../config'
-import { listenToPagamentoEvents, listenToEntregaEvents } from '../events/entrega.consumer'
+import { listenToPagamentoEvents } from '../events/entrega.consumer'
 
 let channel: any
 let connection: any
@@ -8,21 +8,17 @@ let connection: any
 export const initializeRabbitMQ = async (): Promise<void> => {
   try {
     connection = await amqp.connect(config.rabbitmq.url)
-    const ch = await connection.createChannel()
-    channel = ch as unknown as amqp.Channel
+    channel = await connection.createChannel()
 
     await channel.assertExchange('saga.events', 'fanout', { durable: true })
 
     await channel.assertQueue('entrega.events', { durable: true })
-    await channel.assertQueue('entrega.compensation', { durable: true })
 
     await channel.bindQueue('entrega.events', 'saga.events', '')
-    await channel.bindQueue('entrega.compensation', 'saga.events', '')
 
     console.log('[Entrega Service] RabbitMQ conectado.')
 
     listenToPagamentoEvents()
-    listenToEntregaEvents()
   } catch (error) {
     console.error('[Entrega Service] Erro ao conectar ao RabbitMQ:', error)
     setTimeout(initializeRabbitMQ, 5000)
